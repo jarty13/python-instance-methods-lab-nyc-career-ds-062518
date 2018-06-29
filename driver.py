92957d1beae9fafc84ddc20973e9b037b4f8c138
Jennifer Arty <jenniferarty@Jennifers-MacBook-Pro.local>
2018-06-29 14:59:46 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,5 +1,9 @@
 # define Driver class and instance methods here
-
+class Driver:
+    def greeting(self):
+        return "Hey, how are you?"
+    def ask_for_destination(self):
+        return "Where would you like to go today?"
 # Note: remember that our instance methods need to be defined with the an
 # argument in order to be called on an instance of the class or else we will
 # get a `TypeError` telling us that we gave one too many arguments to the
